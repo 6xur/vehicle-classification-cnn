bbbb31b57b5bea2a36853e03da77e5f9fa106974
Robert Xu <u7279920@anu.edu.au>
2023-09-20 17:20:20 +1000

    Refactor code

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import os
 import torch
 import torch.nn as nn
 
-# Hyper-parameters
+# Hyperparameters
 num_epochs = 1
 batch_size = 4
 learning_rate = 0.001
 
 train_directory = "vehicle-x/train"
-
-train_list = []
-vehicle_ids = []
-
 
 class CustomDataset():
     def __init__(self, data_array, labels):
@@ -25,32 +21,36 @@ class CustomDataset():
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
 
-i = 0
-# Load all npy files into a single Numpy array
-for file_name in os.listdir(train_directory):
+train_data = []
+vehicle_ids = []
 
-    # Limit elements in Tensor for testing, to be removed
+# Load numpy arrays and labels from the training directory
+for i, file_name in enumerate(os.listdir(train_directory)):
+
+    # Limit elements in Tensor for testing, remove this when ready for full-scale training
     if(i == 12):
         break
-    i = i + 1
 
     # Extract vehicle ID from the filename
     vehicle_id = int(file_name.split('_')[0])
     vehicle_ids.append(vehicle_id)
 
-    # Load npy file and append to list
+    # Load npy file
     file_path = os.path.join(train_directory, file_name)
     loaded_data = np.load(file_path)
+
+    # Reshape loaded data and append to list
     # TODO: Need to check if this reshape is correct. What is the colour channel?
     reshaped_data = loaded_data.reshape((1, 32, 64))
-    train_list.append(reshaped_data)
+    train_data.append(reshaped_data)
 
-train_array = np.array(train_list)
-train_tensor = torch.from_numpy(train_array)
-
+# Converting training data and labels to PyTorch Tensor
+train_data = np.array(train_data)
+train_data = torch.from_numpy(train_data)
 vehicle_ids = torch.Tensor(vehicle_ids)
 
-train_dataset = CustomDataset(train_tensor, vehicle_ids)
+# Create custom dataset from the tensors
+train_dataset = CustomDataset(train_data, vehicle_ids)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
